1dce444775ca5a545f43a593823ca982c89352e0
Rsoto95 <roberto-soto.com@hotmail.com>
2022-01-21 16:46:40 -0600

    uodating anchor tags

--- src/Components/FollowUs/followUs.js
+++ src/Components/FollowUs/followUs.js
@@ -10,17 +10,17 @@ export function FollowUs(){
     return(
         <section className="follow-us-container">
 
-            <h1>La Mejor Fighting game house en San pedro Sula</h1>
+            <h1>La Fighting game house en San pedro Sula</h1>
             <div className="follow-us">
             <div className="follow-us-text">
             </div>
             </div>
 
             <nav className="socialMediaIconContainer">
-               <a><img className="socialMediaIcon" src={facebookIcon}/></a> 
-               <a><img className="socialMediaIcon youtubeIcon" src={youtubeIcon}/></a> 
-               <a><img className="socialMediaIcon" src={twitchIcon}/></a> 
-               <a><img className="socialMediaIcon" src={twitterIcon}/></a> 
+               <a href="https://www.facebook.com/LegacySmashHN/" target="_blank" ><img className="socialMediaIcon" src={facebookIcon} /></a> 
+               <a href="https://www.youtube.com/channel/UCZ5HmtsUwim6bubEkSm8veg" target="_blank" ><img className="socialMediaIcon youtubeIcon" src={youtubeIcon}/></a> 
+               <a href="https://www.twitch.tv/legacysmashhn" target="_blank" ><img className="socialMediaIcon" src={twitchIcon} /></a> 
+               <a href="https://twitter.com/LegacySmashHN" target="_blank" ><img className="socialMediaIcon" src={twitterIcon}/></a> 
             </nav>
 
         </section>
