dcdec28a97c8cf55696c48d7d138f7c80dcfd468
Rsoto95 <roberto-soto.com@hotmail.com>
2022-05-04 16:28:46 -0600

    Merge logic to grab Monthly and Rank tournaments

--- src/Components/Ranking/calculatingRanking.js
+++ src/Components/Ranking/calculatingRanking.js
@@ -1,6 +1,6 @@
 import { calculation } from "./calculation";
 
-export const RankingTable = async (afterDate, beforeDate,ownerId) => {
+export const RankingTable = async (afterDate, beforeDate, ownerId) => {
   let array = calculation(afterDate, beforeDate, ownerId);
 
   let finalRanking = array.then((r) => {
@@ -11,65 +11,118 @@ export const RankingTable = async (afterDate, beforeDate,ownerId) => {
         TournamentName: [],
         placings: [],
       };
-      
 
-      let puntaje = 0;
-      let participantsVariable=1;
+      let puntajeweekly = [];
+      let puntajeMensual = [];
+      let participantsVariable = 1;
+
+      /*
+      TypeOfTournament va a definir si el torneo es mensual o o no y en base a eso se va a meter en el array
+      correspondiente hay que hacer un if y buscar un indicador ya se por nombre o por hashtag para saber si el torneo es mensual o no.
+      por los momentos todos van a un mismo array que es el array de puntajeweekly
+      */
 
       for (let i = 0; i < a.Tournaments.length; i++) {
+        let typeOfTournament;
 
-        let attendees = a.Tournaments[i].participants[0].length;
-        let topsAmmount=a.Tournaments[i].tops
-        
-        if (attendees<=30&&topsAmmount>=3) {
-          participantsVariable = 1  ;
-        } else if (attendees >30&&topsAmmount>=10 ) {
-          participantsVariable = 1.25;
-        } else if (attendees > 60 && topsAmmount>=14) {
-          participantsVariable = 1.5;
-        } else if (attendees>80 && topsAmmount>=20) {
-          participantsVariable = 1.75;
-        }else if(attendees>100 && topsAmmount>=20){
-          participantsVariable=2;
+        if (
+          a.Tournaments[i].name === "Holy Weekly" ||
+          a.Tournaments[i].name === "Termina 2"
+        ) {
+          typeOfTournament = puntajeMensual;
+        } else {
+          typeOfTournament = puntajeweekly;
         }
 
-        
+        let attendees = a.Tournaments[i].participants[0].length;
+        let topsAmmount = a.Tournaments[i].tops;
+
+        if (attendees <= 30 && topsAmmount >= 3) {
+          participantsVariable = 1;
+        } else if (attendees > 30 && topsAmmount >= 10) {
+          participantsVariable = 1.25;
+        } else if (attendees > 60 && topsAmmount >= 14) {
+          participantsVariable = 1.5;
+        } else if (attendees > 80 && topsAmmount >= 20) {
+          participantsVariable = 1.75;
+        } else if (attendees > 100 && topsAmmount >= 20) {
+          participantsVariable = 2;
+        }
 
         switch (a.Tournaments[i].placement) {
           case 1:
-            puntaje = puntaje + 15*participantsVariable;
+            typeOfTournament.push(15 * participantsVariable);
             break;
           case 2:
-            puntaje = puntaje + 12*participantsVariable;
+            typeOfTournament.push(12 * participantsVariable);
             break;
           case 3:
-            puntaje = puntaje + 10*participantsVariable;
+            typeOfTournament.push(10 * participantsVariable);
             break;
           case 4:
-            puntaje = puntaje + 9*participantsVariable;
+            typeOfTournament.push(9 * participantsVariable);
             break;
           case 5:
-            puntaje = puntaje + 8*participantsVariable;
+            typeOfTournament.push(8 * participantsVariable);
             break;
           case 7:
-            puntaje = puntaje + 6.5*participantsVariable;
+            typeOfTournament.push(6.5 * participantsVariable);
             break;
           case 9:
-            puntaje = puntaje + 4.5*participantsVariable;
+            typeOfTournament.push(4.5 * participantsVariable);
             break;
           case 13:
-            puntaje = puntaje + 3*participantsVariable;
+            typeOfTournament.push(3 * participantsVariable);
             break;
 
-            
           default:
             break;
         }
+
+        let puntajeTorneoRankeado = [];
+
+        if (puntajeweekly.length > 5) {
+          puntajeweekly
+            .sort((a, b) => b - a)
+            .splice(-5)
+            .map((x) => {
+              puntajeTorneoRankeado.push(x);
+            });
+        } else {
+          puntajeweekly
+            .sort((a, b) => b - a)
+            .map((x) => {
+              puntajeTorneoRankeado.push(x);
+            });
+        }
+
+        if (puntajeMensual.length >= 3) {
+          puntajeMensual
+            .sort((a, b) => b - a)
+            .splice(-3)
+            .map((o) => {
+              puntajeTorneoRankeado.push(o);
+            });
+        } else {
+          puntajeMensual
+            .sort((a, b) => b - a)
+            .map((o) => {
+              puntajeTorneoRankeado.push(o);
+            });
+        }
+
+        let sumaPuntaje = 0;
+        if (i === a.Tournaments.length - 1) {
+          for (let h = 0; h < puntajeTorneoRankeado.length; h++) {
+            sumaPuntaje = sumaPuntaje + puntajeTorneoRankeado[h];
+          }
+        }
+
         obj = {
           name: a.GamerTag,
           TournamentName: [...obj.TournamentName, a.Tournaments[i].name],
           placings: [...obj.placings, a.Tournaments[i].placement],
-          score: puntaje,
+          score: sumaPuntaje,
         };
       }
 
